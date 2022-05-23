75c0046f3dca92b2e11098f2fe293d61ae41d30f
mvvassilev <m.v.vassilev@abv.bg>
2022-05-23 14:45:34 +0300

    Add video_input_path as a cli argument

--- audiotovid.py
+++ audiotovid.py
@@ -1,11 +1,13 @@
 from cli import CLI
 from voicetotext import VoiceToTextConverter
+from texttovideo import TextToVideoConverter
 
 __author__ = "Momchil Vasilev (mvvassilev)"
 __version__ = "1.0.0"
 
 audio_input_path = CLI.args.audio_path
-video_output_path = CLI.args.video_path
+video_input_path = CLI.args.video_in_path
+video_output_path = CLI.args.video_out_path
 
 if __name__ == "__main__":
     print(f'Converting file {audio_input_path} to subtitle file...')
--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ import argparse
 class CLI:
     parser = argparse.ArgumentParser(description='An audio to video converter')
     parser.add_argument('audio_path', help='Path to the audio file')
+    parser.add_argument('video_in_path', help='Path to the input video file')
     parser.add_argument(
-        '--video_path', help='Path to store video output', default='.')
+        '--video_out_path', help='Path to store video output', default='.')
     args = parser.parse_args()
