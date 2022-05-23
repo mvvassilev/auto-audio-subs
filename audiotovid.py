5b090155bf19cf26de34aeb906c90a2f1964db4b
mvvassilev <m.v.vassilev@abv.bg>
2022-05-23 14:29:14 +0300

    Isolate audio to text converstion into voicetotext.py

--- audiotovid.py
+++ audiotovid.py
@@ -1,5 +1,5 @@
 from cli import CLI
-import speech_recognition as sr
+from voicetotext import VoiceToTextConverter
 
 __author__ = "Momchil Vasilev (mvvassilev)"
 __version__ = "1.0.0"
@@ -8,17 +8,7 @@ audio_input_path = CLI.args.audio_path
 video_output_path = CLI.args.video_path
 
 if __name__ == "__main__":
-
     print(f'Converting file {audio_input_path} to subtitle file...')
-    try:
-        recognizer = sr.Recognizer()
-        with sr.AudioFile(audio_input_path) as audio:
-            recognizer.adjust_for_ambient_noise(audio)
-            audiodata = recognizer.record(audio)
-            try:
-                text = recognizer.recognize_google(  # TODO: use bing
-                    audio_data=audiodata, language='en-US')
-            except Exception as e:
-                print(e)
-    except Exception as e:
-        print(e)
+    audio_converter = VoiceToTextConverter()
+    text = audio_converter.convert_to_text(audio_input_path=audio_input_path)
+    print(text)
---
+++ voicetotext.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+class VoiceToTextConverter:
+    
+    def __init__(self) -> None:
+        self.recognizer = sr.Recognizer()
+
+    def convert_to_text(self, audio_input_path):
+        try:
+            with sr.AudioFile(audio_input_path) as audio:
+                self.recognizer.adjust_for_ambient_noise(audio)
+                audiodata = self.recognizer.record(audio)
+            try:
+                text = self.recognizer.recognize_google(  # TODO: use bing
+                    audio_data=audiodata, language='en-US')
+            except Exception as e:
+                print(e)
+        except Exception as e:
+            print(e)
+        return text
