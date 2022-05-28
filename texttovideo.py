d8975b1836f242d7ebffaec08631053485371315
mvvassilev <m.v.vassilev@abv.bg>
2022-05-28 17:22:07 +0300

    Changest to texttovid.py to format better

--- texttovideo.py
+++ texttovideo.py
@@ -1,3 +1,4 @@
+from time import sleep
 import cv2
 import os
 import numpy as np
@@ -29,58 +30,22 @@ class TextToVideoConverter:
         height, width, layers = video_bg.shape
         size = (width, height)
         # TEMP TEXT FOR TESTING PURPOSES
-        text = """
-            Contrary to popular belief, Lorem Ipsum is not simply random text. 
-            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
-            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
-            and going through the cites of the word in classical literature, discovered the 
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
-            written in 45 BC. This book is a treatise on the theory of ethics, very 
-            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
-            sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random text. 
-            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
-            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
-            and going through the cites of the word in classical literature, discovered the 
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
-            written in 45 BC. This book is a treatise on the theory of ethics, very 
-            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
-            sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random text. 
-            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
-            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
-            and going through the cites of the word in classical literature, discovered the 
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
-            written in 45 BC. This book is a treatise on the theory of ethics, very 
-            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
-            sit amet..", comes from a line in section 1.10.32.
-            Contrary to popular belief, Lorem Ipsum is not simply random text. 
-            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
-            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
-            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
-            and going through the cites of the word in classical literature, discovered the 
-            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
-            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
-            written in 45 BC. This book is a treatise on the theory of ethics, very 
-            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
-            sit amet..", comes from a line in section 1.10.32.
-        """
+        text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,"
 
         x, y = 0, 5
 
         vid_writer = cv2.VideoWriter(
             video_output_path, cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
         text_list = self.text_splitter.split_into_sentences(text)
-        for frame_text in text_list:
+        sentence_iterator = iter(text_list)
+        for sentence in sentence_iterator:
+            video_bg = cv2.imread(self.background)
+            sentence += next(sentence_iterator, "") + next(sentence_iterator, "") + \
+                next(sentence_iterator, "") + \
+                next(sentence_iterator, "") + \
+                next(sentence_iterator, "")
             # TODO: make width dynamic
-            for i, line in enumerate(textwrap.wrap(frame_text, width=50)):
-                video_bg = cv2.imread(self.background)
+            for i, line in enumerate(textwrap.wrap(sentence, width=80)):
                 textsize = cv2.getTextSize(
                     line, heading_font, heading_size, heading_weight)[0]
 
@@ -98,6 +63,7 @@ class TextToVideoConverter:
                             cv2.LINE_AA)
                 # create a temp image with text
                 cv2.imwrite(f'dbg/TEMP_{i}.png', video_bg)
+            for _ in range(70):  # TODO: change frames to corr to audio lenght
                 vid_writer.write(cv2.imread(f'dbg/TEMP_{i}.png'))
 
         vid_writer.release()
