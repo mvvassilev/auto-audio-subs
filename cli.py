59bf96181855b40f53888c797c5382f8f25f4216
mvvassilev <m.v.vassilev@abv.bg>
2022-05-28 15:32:18 +0300

    Splitting text v1.0, storring frames into tmp file_frames v1.0

--- cli.py
+++ cli.py
@@ -1,10 +1,11 @@
 import argparse
+import os
 
 
 class CLI:
     parser = argparse.ArgumentParser(description='An audio to video converter')
     parser.add_argument('audio_path', help='Path to the audio file')
-    parser.add_argument('video_in_path', help='Path to the input video file')
+    parser.add_argument('--background', help='Path to background image', default='background_default.png')
     parser.add_argument(
-        '--video_out_path', help='Path to store video output', default='.')
+        '--video_out_path', help='Path to store video output', default=f'{os.getcwd()}/video.mp4')
     args = parser.parse_args()
--- texttovideo.py
+++ texttovideo.py
@@ -1,9 +1,17 @@
 import cv2
+import os
+import numpy as np
 from progressbar import ProgressBar
+from textsplitter import TextSplitter
+import textwrap
 
 heading_weight = 1
-title_weight = 2
+title_weight = 1
 body_weight = 1
+
+heading_size = 1
+title_size = 1
+body_size = 1
 
 heading_font = cv2.FONT_HERSHEY_COMPLEX
 title_font = cv2.FONT_HERSHEY_TRIPLEX
@@ -12,57 +20,86 @@ body_font = cv2.FONT_HERSHEY_COMPLEX
 
 class TextToVideoConverter:
 
-    def __init__(self, video_input_path) -> None:
-        self.video_input_path = video_input_path
-        self.cap = cv2.VideoCapture(self.video_input_path)
+    def __init__(self, background) -> None:
+        self.background = background
+        self.text_splitter = TextSplitter()
 
-    def capture(self, text):
-        progress_bar = ProgressBar(self.cap)
-        (fps, frame_count, durationSec) = progress_bar.getStats()
+    def capture(self, text, video_output_path):
+        video_bg = cv2.imread(self.background)
+        height, width, layers = video_bg.shape
+        size = (width, height)
+        # TEMP TEXT FOR TESTING PURPOSES
+        text = """
+            Contrary to popular belief, Lorem Ipsum is not simply random text. 
+            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+            and going through the cites of the word in classical literature, discovered the 
+            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
+            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
+            written in 45 BC. This book is a treatise on the theory of ethics, very 
+            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
+            sit amet..", comes from a line in section 1.10.32.
+            Contrary to popular belief, Lorem Ipsum is not simply random text. 
+            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+            and going through the cites of the word in classical literature, discovered the 
+            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
+            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
+            written in 45 BC. This book is a treatise on the theory of ethics, very 
+            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
+            sit amet..", comes from a line in section 1.10.32.
+            Contrary to popular belief, Lorem Ipsum is not simply random text. 
+            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+            and going through the cites of the word in classical literature, discovered the 
+            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
+            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
+            written in 45 BC. This book is a treatise on the theory of ethics, very 
+            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
+            sit amet..", comes from a line in section 1.10.32.
+            Contrary to popular belief, Lorem Ipsum is not simply random text. 
+            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. 
+            Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
+            looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+            and going through the cites of the word in classical literature, discovered the 
+            undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 
+            "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, 
+            written in 45 BC. This book is a treatise on the theory of ethics, very 
+            popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor 
+            sit amet..", comes from a line in section 1.10.32.
+        """
 
-        while(True):
-            # Capture frames in the video
-            ret, currentframe = self.cap.read()
+        x, y = 0, 5
 
-            # Use putText() method for
-            # inserting text on video
-            cv2.putText(currentframe,
-                        text,
-                        (50, 50),
-                        heading_font, 1,
-                        (0, 0, 0),  # rgb
-                        heading_weight,
-                        cv2.LINE_4)
+        vid_writer = cv2.VideoWriter(
+            video_output_path, cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+        text_list = self.text_splitter.split_into_sentences(text)
+        for frame_text in text_list:
+            # TODO: make width dynamic
+            for i, line in enumerate(textwrap.wrap(frame_text, width=50)):
+                video_bg = cv2.imread(self.background)
+                textsize = cv2.getTextSize(
+                    line, heading_font, heading_size, heading_weight)[0]
 
-            cv2.putText(currentframe,
-                        text,
-                        (50, 90),
-                        title_font, 1,
-                        (0, 0, 0),  # rgb
-                        title_weight,
-                        cv2.LINE_4)
+                gap = textsize[1] + 10
 
-            cv2.putText(currentframe,
-                        text,
-                        (50, 130),
-                        body_font, 1,
-                        (0, 0, 0),  # rgb
-                        body_weight,
-                        cv2.LINE_4)
+                y = int((video_bg.shape[0] + textsize[1]) / 2) + i * gap
+                x = int((video_bg.shape[1] - textsize[0]) / 2)
 
-            if currentframe is not None:
+                cv2.putText(video_bg,
+                            line,
+                            (x, y),
+                            heading_font, 1,
+                            (0, 0, 0),  # rgb
+                            heading_weight,
+                            cv2.LINE_AA)
+                # create a temp image with text
+                cv2.imwrite(f'dbg/TEMP_{i}.png', video_bg)
+                vid_writer.write(cv2.imread(f'dbg/TEMP_{i}.png'))
 
-                # display progress bar after saving video so that it does not ge saved with
-                # current frame
-                currentframe = progress_bar.displayProgressBar(currentframe)
-                # display video stream
-                cv2.imshow('video', currentframe)
-            else:
-                break
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-        # release the cap object
-        self.cap.release()
-        # close all windows
+        vid_writer.release()
+        os.system('rm -rf dbg/TEMP_*.png')  # remove temp image
         cv2.destroyAllWindows()
