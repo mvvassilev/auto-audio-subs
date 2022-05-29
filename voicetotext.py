2b37041626eab695704febe877c67f5078196c48
mvvassilev <m.v.vassilev@abv.bg>
2022-05-29 10:39:10 +0300

    Handling mp3 format

--- voicetotext.py
+++ voicetotext.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+from pydub import AudioSegment
 
 class VoiceToTextConverter:
     
@@ -6,6 +7,12 @@ class VoiceToTextConverter:
         self.recognizer = sr.Recognizer()
 
     def convert_to_text(self, audio_input_path) -> str:
+        #handle mp3 conversion
+        if audio_input_path.lower().endswith('.mp3'):
+            audio = AudioSegment.from_mp3(audio_input_path)
+            audio_input_path = audio_input_path[:-3] + 'wav'
+            audio.export(audio_input_path, format='wav')
+
         try:
             with sr.AudioFile(audio_input_path) as audio:
                 self.recognizer.adjust_for_ambient_noise(audio)
