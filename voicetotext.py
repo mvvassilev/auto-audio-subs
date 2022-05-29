03d2349e1878bb73aed669d7d4cb8aa63d619dab
mvvassilev <m.v.vassilev@abv.bg>
2022-05-29 17:46:30 +0300

    Remove TODO's

--- voicetotext.py
+++ voicetotext.py
@@ -1,13 +1,14 @@
 import speech_recognition as sr
 from pydub import AudioSegment
 
+
 class VoiceToTextConverter:
-    
+
     def __init__(self) -> None:
         self.recognizer = sr.Recognizer()
 
     def convert_to_text(self, audio_input_path) -> str:
-        #handle mp3 conversion
+        # handle mp3 conversion
         if audio_input_path.lower().endswith('.mp3'):
             audio = AudioSegment.from_mp3(audio_input_path)
             audio_input_path = audio_input_path[:-3] + 'wav'
@@ -18,7 +19,7 @@ class VoiceToTextConverter:
                 self.recognizer.adjust_for_ambient_noise(audio)
                 audiodata = self.recognizer.record(audio)
             try:
-                text = self.recognizer.recognize_google(  # TODO: use bing
+                text = self.recognizer.recognize_google(
                     audio_data=audiodata, language='en-US')
             except Exception as e:
                 print(e)
